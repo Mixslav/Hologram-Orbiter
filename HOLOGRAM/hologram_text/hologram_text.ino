73c40b916503ef24c6fb1adf56833dbf4daaf5bf
Mislav Stipić <38721847+Mixslav@users.noreply.github.com>
2022-05-10 15:08:19 +0200

    Update hologram_text.ino

--- HOLOGRAM/hologram_text/hologram_text.ino
+++ HOLOGRAM/hologram_text/hologram_text.ino
@@ -1,8 +1,6 @@
 #include <hologram.h>   // library for holografic device
 #include "icon.h"         // data base for letters and numbers
-#include "scy_fi_block.h" // data base for animation
-#include "equalizer.h"    // data base for animation
-#include "my_gif.h"       // data base for animation
+
 // these pins are for data transfer
 //#define SPICLOCK  13//sck
 //#define DATAOUT 11//MOSI
@@ -13,14 +11,14 @@
 hologram lcd(6,7,5,4);  
 
   
-const int B_LED_1 = 9;      // blue leds
+const int B_LED_1 = 9;      // blue leds, you can solder these leds on 4,4 V before diode nd arduino
 const int B_LED_2 = 10;         
 
 
-// here are numbers for manual synking both LED boards ( it is a number around 0 and 500)
+// here are numbers for manual synking both LED boards ( it is a number around 0 and 2000, 300 is the lenght of 1 pixel)
 
-unsigned int br_sinkL = 0;   // variable for first LED board synchronization
-unsigned int br_sinkR = 110; // variable for second LED board synchronization
+unsigned int br_sinkL = 0;   // variable for first LED board synchronization   (1500)
+unsigned int br_sinkR = 50; // variable for second LED board synchronization  (350)
 
 
 // variable for controlling the lenght of all pixels (it is usually around 300)
@@ -54,12 +52,12 @@ void loop() {
 
 lcd.delete_m(); // delete matrix to ensure there are no extra symbols
 lcd.text_load("HOLOGRAM");            // write whatever
-lcd.print_timed_matrtx(7000,1);       //project text for 7 seconds, 1 is for slide, 0 is without slide
+lcd.print_m(7000,0);       //project text for 7 seconds, 1 is for slide, 0 is without slide
 
 lcd.delete_m(); // delete matrix to ensure there are no extra symbols
-lcd.text_load("ORBITER");   // write whatever
+lcd.text_load("  ORBITER");   // write whatever (mind the spaces I left them so you can read the text from start)
 lcd.inverse_pixel();        // inverse all pixels (all except gifs)
-lcd.print_timed_matrtx(7000,1);       //project text for 7 seconds, 1 is for slide, 0 is without slide
+lcd.print_m(7000,1);       //project text for 7 seconds, 1 is for slide, 0 is without slide
 
 }
 
